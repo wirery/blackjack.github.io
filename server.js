2f1fb1f722f29ea7f5cb13f46358b46a58f3aaa7
wirery <wire_ryan@yahoo.com>
2021-06-02 16:57:38 -0400

    Server.js updated for new.ejs get and push

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+
+app.get('/coins/new', (req, res) => {
+    res.render('new.ejs');
+  })
+  app.get('/coins', (req, res) => {
+    res.render('index.ejs', {
+      coins: coins
+    })
+  })
+
+app.post('/coins', (req, res) => {
+    console.log(req.body)
+    coins.push(req.body)
+    res.redirect('/coins')
+  })
+
 app.listen(PORT, () => {
     console.log ("Server is running on port " + PORT)
 })

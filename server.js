e0b1380550f1b6a29725fcd70e94fa350cafdfe3
wirery <wire_ryan@yahoo.com>
2021-06-02 22:45:10 -0400

    New page functioning, and console receiving data;mongoose installed

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({extended: true})) 
 
 app.get('/coins/new', (req, res) => {
     res.render('new.ejs');
   })
   app.get('/coins', (req, res) => {
     res.render('index.ejs', {
-      coins: coins
+    //   coins: coins
     })
   })
 
 app.post('/coins', (req, res) => {
     console.log(req.body)
-    coins.push(req.body)
-    res.redirect('/coins')
+    // coins.push(req.body)
+    res.send('received')
+    // res.redirect('/coins')
   })
 
 app.listen(PORT, () => {

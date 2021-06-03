116a5755a7f54e6191ac3de7d73dd2c0213567c4
wirery <wire_ryan@yahoo.com>
2021-06-02 23:51:30 -0400

    Coins.js begun, coin schema made

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 const app = express()
 const PORT = 3000
 
 app.use(express.urlencoded({extended: true})) 
+
+
+mongoose.connect('mongodb://localhost:27017/coincrud', {useNewUrlParser: true})
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo')
+})
 
 app.get('/coins/new', (req, res) => {
     res.render('new.ejs');

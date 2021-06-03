6808279b8d7ead8251dd1b1d60a3e7820470b37b
wirery <wire_ryan@yahoo.com>
2021-06-03 12:36:42 -0400

    Create object page renders onto index page

--- models/coins.js
+++ models/coins.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 const coinSchema = new mongoose.Schema({
     country: { type: String, required: true},
-    // img:  { type: String, required: true},
-    // denomination:  { type: String, required: true},
-    // quantity:  { type: String, required: true},
-    // year:  { type: String, required: true},
-    // dateOfPurchase:  { type: String, required: true},
-    // dateSold:  { type: String, required: true},
-    // salePrice:  { type: String, required: true}
+    img:  { type: String, required: true},
+    denomination:  { type: String, required: true},
+    quantity:  { type: String, required: true},
+    year:  { type: String, required: true},
+    dateOfPurchase:  { type: String, required: true},
+    dateSold:  { type: String, required: true},
+    salePrice:  { type: String, required: true}
     
 })
 
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
 
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }))
 
 
-mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
     console.log('connected to mongo')
 })
@@ -20,37 +20,33 @@ const Coin = require('./models/coins.js')
 
 
 app.get('/coins', (req, res) => {
-    Coin.find({},(error, allCoins)=> {
+    // console.log(res.data)
+    Coin.find({}, (error, allCoins) => {
         if (error) {
-            res.send(error)
+            console.log(error)
         } else {
             res.render('index.ejs', {
-               coins: allCoins 
+                Coin: allCoins
             })
         }
-        res.send(allCoins)
     })
-    // res.render('index.ejs'
-    //, {
-    // //   Coin: Coin
-    // })
-  })
+})
 
 app.get('/coins/new', (req, res) => {
     res.render('new.ejs');
-  })
-  
+})
+
 
 app.post('/coins', (req, res) => {
     console.log(req.body)
     // Coin.push(req.body)
     Coin.create(req.body, (error, createdCoin) => {
-        res.send(createdCoin)
+        // res.redirect(createdCoin)
     })
     // res.send('received')
-    // res.redirect('/coins')
-  })
+    res.redirect('/coins')
+})
 
 app.listen(PORT, () => {
-    console.log ("Server is running on port " + PORT)
+    console.log("Server is running on port " + PORT)
 })

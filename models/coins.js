6e927e252e1951b0eb9075e23b57327e403eb67f
wirery <wire_ryan@yahoo.com>
2021-06-04 07:22:47 -0400

    Comment added to index page

--- models/coins.js
+++ models/coins.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 const coinSchema = new mongoose.Schema({
-    country: { type: String, required: true},
-    img:  { type: String, required: true},
-    year:  { type: Number, required: true},
-    quantity:  { type: Number, required: true},
-    grade:  { type: Number, required: true},
-    value:  { type: Number, required: true}
-    
+    country: { type: String, required: true },
+    img: { type: String, required: true },
+    year: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    grade: { type: Number, required: true },
+    value: { type: Number, required: true }
+
 })
 
 const Coin = mongoose.model('Coins', coinSchema)
